355967377134c77de932205ca9cffd65b933dd6f
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-06-28 21:02:39 +0330

    Create orGroup.js
    Signed-off-by: danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>

---
+++ orGroup.js
@@ -0,0 +1,20 @@
+{
+  "orGroup": {
+    "expressions": [{
+      "filter": {
+        "fieldName":"eventName",
+        "stringFilter": {
+          "value":"page_view"
+        }
+      }
+    },
+    {
+      "filter": {
+        "fieldName":"eventName",
+        "stringFilter": {
+          "value":"first_visit"
+        }
+      }
+    }]
+  }
+}
