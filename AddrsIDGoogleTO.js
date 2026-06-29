4050495fc7cbdb064217c554ce5d661eb014d31f
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-06-29 13:32:29 +0330

    Create AddrsIDGoogleTO.js
    Signed-off-by: danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>

---
+++ AddrsIDGoogleTO.js
@@ -0,0 +1,20 @@
+Addrs ID Google TO (@UserHandle) {
+"user@example.com"
+Password Google 1 on : "TokenA"
+Password Photo 1 on : "TokenB"
+
+Email : @@@@@@@@@@@@@@@@@@@@@@
+address-on.Copy_Lock.Now_Res.ro.
+Now "ran_SystemGPS.IP"
+System_SD_SD-USB.dededt {}
+{
+Command  = Action Object [Qualifier] [Params] ;
+Action      = "Addrs" | "Password" | "Email" | "System" | "Res" ;
+Object     = "ID" | "Google" | "Photo" | "SD" | "USB" ;
+Qualifier   = "TO" | "on" ;
+Params    = String | Identifier | Chain ;
+String      = '"' { any } '"' ;
+Identifier  = '@' { letter | digit } ;
+Chain      = { Identifier '.' } ;      
+ }                        
+}
